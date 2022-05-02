67692ad2659513628058525b4c9df06048d5fcf2
isofttechn <isofttechn@gmail.com>
2022-05-02 12:11:06 +0100

    Updated hardhat.config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
+require('@nomiclabs/hardhat-etherscan');
+
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -17,5 +20,31 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: "0.5.16",
+  networks: {
+    dev: {
+        url: "http://localhost:7545",
+        gasPrice: 20,
+        saveDeployments: true
+    },
+    bsctest: {
+        url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+        accounts: [process.env.PRIV_KEY],
+        gasPrice: 20000000000, 
+        saveDeployments: true,
+        blockGasLimit: 1000000
+    },
+    bsc: {
+        url: "https://bsc-dataseed.binance.org",
+        accounts: [process.env.PRIV_KEY],
+        gasPrice: 20000000000,
+        saveDeployments: true,
+        blockGasLimit: 1000000
+    }
+},
+
+etherscan:{
+  apiKey:process.env.API_KEY,
+}
 };
+
